e3d2cdfbfee9cbc6be5553cbc0454e4c7140dfcb
MayGo <maigo.erit@gmail.com>
2013-06-29 22:48:56 +0300

    Small changes.

--- griffon-app/views/timetracker/TimeTrackerView.groovy
+++ griffon-app/views/timetracker/TimeTrackerView.groovy
@@ -14,35 +14,36 @@ import javax.swing.table.TableColumn
 import javafx.beans.property.SimpleStringProperty;
 import javafx.util.Callback;
 
-def fxmlLoader = new FXMLLoader(app.getResourceAsURL('TimeTracker2.fxml'))
+def fxmlLoader = new FXMLLoader(app.getResourceAsURL('TimeTracker.fxml'))
 def fxmlPane = fxmlLoader.load()
 
+
 application(title: 'TimeTracker', sizeToScene: true, centerOnScreen: true) {
-	scene=scene(fill: lightgray, width: 900, height: 700) {
-		node(fxmlPane)
-
-		
-	}
+	scene=scene(fill: lightgray, width: 900, height: 700) { node(fxmlPane) }
 	noparent {
-		loadDataButton=fxmlLoader.getNamespace().get("loadDataButton")
+		[
+			"loadDataButton",
+			"infoTable",
+			"nameCol",
+			"titleCol",
+			"dateCreatedCol"
+		].each{name->
+			this."$name"=fxmlLoader.getNamespace().get(name);
+		}
 		loadDataButton.onAction(controller.loadData)
-		infoTable=fxmlLoader.getNamespace().get("infoTable")
-		nameColumn=fxmlLoader.getNamespace().get("nameCol")
-		titleColumn=fxmlLoader.getNamespace().get("titleCol")
-		dateCreatedColumn=fxmlLoader.getNamespace().get("dateCreatedCol")
-		
-		nameColumn.setCellValueFactory(new PropertyValueFactory<ActiveWindowInfo, String>("name"));
-		titleColumn.setCellValueFactory(new PropertyValueFactory<ActiveWindowInfo, String>("title"));
-		dateCreatedColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ActiveWindowInfo, String>, ObservableValue<String>>() {
 
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<ActiveWindowInfo, String> param) {
-				// The creation date will never change...
-				ActiveWindowInfo awi = param.getValue();
-				
-				return new SimpleStringProperty(awi.dateCreated);
-			}
-		});
+		nameCol.setCellValueFactory(new PropertyValueFactory<ActiveWindowInfo, String>("name"));
+		titleCol.setCellValueFactory(new PropertyValueFactory<ActiveWindowInfo, String>("title"));
+		dateCreatedCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ActiveWindowInfo, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<ActiveWindowInfo, String> param) {
+						// The creation date will never change...
+						ActiveWindowInfo awi = param.getValue();
+
+						return new SimpleStringProperty(awi.dateCreated);
+					}
+				});
 		model.activeWindowInfoList.addListener({
 			infoTable.items.clear()
 			infoTable.items.addAll(model.activeWindowInfoList)
